ab1bf6fe0782eb04c1e0ff82d38bf3fa9600a680
divya <radichow2@gmail.com>
2020-12-09 15:02:40 -0600

    if this applied, this commit will apply IIFE to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,48 @@
-let pokemonList = [{
-    name: 'Balbasaur',
-    types: ['grass', 'poison'],
-    height: 0.7
-  },
-  {
-    name: 'Pidgeottlo',
-    types: ['flying','normal'],
-    height: 1.6
-  },
-  {
-    name: 'Slowpoke',
-    types: ['psychic', 'water'],
-    height: 1.2
-  },
-  {
-    name: 'Yamma',
-    types: ['bug', 'flying'],
-    height: 1.2
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Balbasaur",
+      types: ["grass", "poison"],
+      height: 0.7
+    },
+    {
+      name: "Pidgeottlo",
+      types: ["flying", "normal"],
+      height: 1.6
+    },
+    {
+      name: "Slowpoke",
+      types: ["psychic", "water"],
+      height: 1.2
+    },
+    {
+      name: "Yamma",
+      types: ["bug", "flying"],
+      height: 1.2
+    }
+  ];
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
 
-pokemonList.forEach(function(user){
-  if (user.height> 1.5){
-  document.write(user.name+' (Height:' + user.height +' - wowww this is big one!'+'</br>')
-}
-else{
-  document.write(user.name + ' (Height: ' + user.height + ')'+'<br/>')
+  function getAll() {
+    return pokemonList;
+  }
 
-}
-});
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+console.log(pokemonRepository.add({name:'kimichi', height:1.3}));
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(user) {
+      if (user.height > 1.5) {
+        document.write(user.name + ' (Height:' + user.height + ' - wowww this is big one!' + '</br>')
+      } else {
+        document.write(user.name + ' (Height: ' + user.height + ')' + '<br/>')
+
+      }
+    });
